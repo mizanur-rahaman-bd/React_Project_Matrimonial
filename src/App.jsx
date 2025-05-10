3748504276a6f0338f7a8b899200c0fdf75218ed
mizanur-rahaman-bd <sumon1581@gmail.com>
2025-05-10 21:26:28 +0600

    icon_Set

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-    <h1 className='bg-amber-400'>dfhdrhrd</h1>
-      
+      <div>
+        <div className="container">
+          <h1 className="bg-amber-400">dfhdrhrd</h1>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
